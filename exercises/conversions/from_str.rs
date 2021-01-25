587da5ddd5f77bb0ef0abbbdcb09a9b54c8cbc59
Bo Lu <bo.lu@utexas.edu>
2021-01-24 20:37:22 -0800

    finished!

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,17 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let parts = s.splitn(2, ',').collect::<Vec<&str>>();
+
+        match &parts[..] {
+            [name, age] if !name.is_empty() => age
+                .parse()
+                .map(|age| Ok(Person {
+                    name: name.to_string(),
+                    age}))
+                .unwrap(),
+            _ => Err("Cannot parse person".into()),
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -11,7 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -25,19 +24,34 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b) = tuple;
+
+        match (u8::try_from(r), u8::try_from(g), u8::try_from(b))
+        {
+            (Ok(red),Ok(green), Ok(blue)) => Ok(Color{red, green, blue}),
+            _ => Err(String::from("Failed to convert!"))
+        }
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Color::try_from((arr[0], arr[1], arr[2]))
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = String;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        match slice {
+            [r, g, b] => Color::try_from((*r,*g,*b)),
+            _ => Err(String::from("Failed to convert"))
+        }
+    }
 }
 
 fn main() {
